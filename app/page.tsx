e9abb4b8c917d29fb69a8a177f383c4f9d55f6ab
Luca Faggianelli <luca.faggianelli@gmail.com>
2024-08-13 18:37:25 +0200

    improve home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,51 @@
-import PreviewCard from '@/components/PreviewCard'
 import Image from 'next/image'
 
+import PreviewCard from '@/components/PreviewCard'
+
 interface DemoItem {
-  id: string
   title?: string
   titleColor?: string
-  image?: JSX.Element
+  image?: string
+  href: string
 }
 
 const demos: DemoItem[] = [
   {
-    id: 'waves-1',
-    title: 'Fresh coffee',
+    title: 'Waves',
     titleColor: '#eae6cd',
-  },
-  {
-    id: 'waves-2',
-    title: '7x Sardines',
-    titleColor: '#f6f6f6',
-  },
-  {
-    id: 'waves-3',
-    title: 'Subscribe',
-    titleColor: '#f6f6f6',
-  },
-  {
-    id: 'waves-text',
-    image: (
-      <h2 className="text-7xl font-serif text-stone-600 text-center">Waves</h2>
-    ),
-  },
-  {
-    id: 'waves-5',
-    title: 'Happy hour',
-    titleColor: 'white',
-  },
-  {
-    id: 'waves-4',
-    title: 'Book a demo',
-    titleColor: '#9f1cd8',
+    href: '/designs/waves',
+    image: 'waves-1.svg',
   },
 ]
 
 export default function Home() {
   return (
-    <main className="container max-w-screen-lg mx-auto py-16 px-8 md:px-16 lg:px-0 grid gap-4 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
-      {demos.map((demo) => (
-        <PreviewCard
-          key={demo.id}
-          title={demo.title}
-          textColor={demo.titleColor}
-        >
-          {demo.image ? (
-            demo.image
-          ) : (
-            <img className="h-full" src={`/demo/${demo.id}.svg`} alt={''} />
-          )}
-        </PreviewCard>
-      ))}
-    </main>
+    <div className="container max-w-screen-sm mx-auto">
+      <section className="pt-8">
+        <h1 className="text-4xl font-medium text-stone-700 mb-1">Squiggle</h1>
+        <h2 className="text-2xl text-stone-400">
+          A collection of customizable designs
+        </h2>
+      </section>
+
+      <main className="py-16 px-8 md:px-16 lg:px-0 grid gap-4 grid-cols-1">
+        {demos.map((demo) => (
+          <PreviewCard
+            key={demo.href}
+            title={demo.title}
+            textColor={demo.titleColor}
+            href={demo.href}
+          >
+            <Image
+              className="h-full"
+              src={`/demo/${demo.image}`}
+              alt={`${demo.title} preview`}
+              width={800}
+              height={300}
+            />
+          </PreviewCard>
+        ))}
+      </main>
+    </div>
   )
 }
--- components/PreviewCard.tsx
+++ components/PreviewCard.tsx
@@ -1,22 +1,35 @@
+import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 
 interface Props extends PropsWithChildren {
   title?: string
   textColor?: string
+  href: string
 }
 
-export default function PreviewCard({ title, textColor, children }: Props) {
+export default function PreviewCard({
+  title,
+  textColor,
+  children,
+  href,
+}: Props) {
   return (
-    <div className="relative rounded-3xl overflow-hidden flex aspect-video">
+    <Link
+      className="relative rounded-3xl overflow-hidden flex aspect-video"
+      href={href}
+    >
       <div className="flex items-center justify-center">{children}</div>
 
       <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center p-16">
         {title && (
-          <h1 className="text-7xl font-serif text-center" style={{ color: textColor }}>
+          <h1
+            className="text-7xl font-serif text-center"
+            style={{ color: textColor }}
+          >
             {title}
           </h1>
         )}
       </div>
-    </div>
+    </Link>
   )
 }
